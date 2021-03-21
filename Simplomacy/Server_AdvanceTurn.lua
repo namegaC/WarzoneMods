332355bb94c1044d604b3a642d1610792b3ceb9d
namegaC <cageman@live.nl>
2021-03-21 13:51:29 +0100

    Relationsmatrix rework, diplo enforcement and possible active cards influencing

--- Simplomacy/Server_AdvanceTurn.lua
+++ Simplomacy/Server_AdvanceTurn.lua
@@ -1,14 +1,33 @@
 require('RelationsMatrix')
 require('DiplomacyOrder')
 
+function PrintProxyInfo(obj)
+	print('type=' .. obj.proxyType .. ' readOnly=' .. tostring(obj.readonly) .. ' readableKeys=' .. table.concat(obj.readableKeys, ',') .. ' writableKeys=' .. table.concat(obj.writableKeys, ','));
+  end
+
+
 
 function Server_AdvanceTurn_Start (game,addNewOrder)
 	playerGameData = Mod.PlayerGameData;
 	publicGameData = Mod.PublicGameData;
 
-    --To test Diplomacy card beginning of turn: (verified: works)
-    DiplomacyOrder(WL, addNewOrder, 1201687, 2)
+	PrintProxyInfo(game.ServerGame.LatestTurnStanding);
+
+    standing = game.ServerGame.LatestTurnStanding;
+
+
+    -- standing = RemoveActiveDiploCardsFromMod(standing, publicGameData.ActiveDiploCardIDsFromMod, game.ServerGame.Game.TurnNumber);
+
+
+
+
+Mod.PublicGameData = publicGameData;
+Mod.PlayerGameData = playerGameData;
+
 end
+
+
+
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 
@@ -18,21 +37,23 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
         -- 1201687 is myself
         -- print(entry[1201687]);
     end
+    local currentRelationMatrix = publicGameData.RelationsMatrix;
 
     --For testing purposes, relations can be changed to "Allied" by playing a Spy card.
     if(order.proxyType == 'GameOrderPlayCardSpy') then
-        publicGameData.RelationsMatrix = ChangedRelations(order.PlayerID, order.TargetPlayerID, publicGameData.RelationsMatrix, "Allied");
+        ProposeChange(currentRelationMatrix, order.PlayerID, order.TargetPlayerID, "Allied");
     end
 
-    --For testing purposes, relations can be changed to "Peace" by playing a Gift card.
+    --For testing purposes, relations can be changed to "War" by playing a Gift card.
     if(order.proxyType == 'GameOrderPlayCardGift') then
-        publicGameData.RelationsMatrix = ChangedRelations(order.PlayerID, order.GiftTo, publicGameData.RelationsMatrix, "Peace");
+        ProposeChange(currentRelationMatrix, order.PlayerID, order.GiftTo, "Peace");
     end
 
-    --For testing purposes, relations can be changed to "War" by playing a Sanctions card.
+    --For testing purposes, relations can be changed to "Peace" by playing a Sanctions card.
     if(order.proxyType == 'GameOrderPlayCardSanctions') then
-        publicGameData.RelationsMatrix = ChangedRelations(order.PlayerID, order.SanctionedPlayerID, publicGameData.RelationsMatrix, "War");
+        ProposeChange(currentRelationMatrix, order.PlayerID, order.SanctionedPlayerID, "War");
     end
+
 
 
     Mod.PublicGameData = publicGameData;
@@ -42,6 +63,22 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 end
 
 function Server_AdvanceTurn_End (game,addNewOrder)
+
+    playerGameData = Mod.PlayerGameData;
+	publicGameData = Mod.PublicGameData;
+
+    ValidateAndCorrectChangedRelations(publicGameData.RelationsMatrix, publicGameData.ProposedRelationChanges);
+
+    EnforceAllModDiplomacies(WL, publicGameData.RelationsMatrix, addNewOrder, publicGameData.ActiveDiploCardIDsFromMod);
+
+    Mod.PublicGameData = publicGameData;
+	Mod.PlayerGameData = playerGameData;
+    
+    --print relations after turn:
+    print('All relations:');
+    for _, relation in pairs(publicGameData.RelationsMatrix) do
+        print(tostring(relation[1]..' '..relation[2]..' '..relation[3]))
+    end
 
 end
 
